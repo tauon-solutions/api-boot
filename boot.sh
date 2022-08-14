7b22be77d2a93e0297e8087577a07810b53da84c
Tauon Solutions <75220693+tauonsolutions@users.noreply.github.com>
2022-08-14 16:02:15 +0200

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -13,7 +13,7 @@ echo "1. Installation completed"
 #################
 echo "2. Cloning the Repo from Github"
 cd /var/www/
-sudo git clone https://ratan.vanjani%40tauon.in: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@github.com/tauon-solutions/letterla-api
+sudo git clone https://github.com/tauon-solutions/letterla-api
 sudo chmod 777 -R letterla-api/
 cd /var/www/letterla-api/
 
